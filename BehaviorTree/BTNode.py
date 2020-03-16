653b04c380f043602cedadd3c17a16a8696c630c
wangyang <dgKira@163.com>
2020-03-17 03:39:05 +0800

    BTTree3

--- BehaviorTree/BTNode.py
+++ BehaviorTree/BTNode.py
@@ -1,33 +1,27 @@
 # coding:utf-8
 
-from BTResult import CBTResult
+from defines import *
 import time
 
 class CBTNode(object):
 
-    def __init__(self):
+    def __init__(self,oPrecondition=None):
         self.m_DataBase=None#共享数据
 
-        self.m_LastCheckTime=0
+        self.m_LastCheckTime=0#上次检查时间
         self.m_Interval=0#冷却
 
-        self.m_Actived=True
-        self.m_Result=CBTResult()
+        self.m_Actived=False
 
-        self.m_Parent=None
-        self.m_Child=[]
-
-    def SetParent(self,oParent):
-        self.m_Parent=oParent
-
-    def Parent(self):
-        return self.m_Parent
+        self.m_Precondition=oPrecondition#前置条件
+        self.m_Child=[]#子节点
 
     def AddChild(self,node):
         self.m_Child.append(node)
 
     def RemoveChild(self,node):
-        self.m_Child.remove(node)
+        if node in self.m_Child:
+            self.m_Child.remove(node)
 
     #配置共享数据
     def Activate(self,oData):
@@ -35,11 +29,12 @@ class CBTNode(object):
             return
         self.m_DataBase=oData
 
-        if self.Parent():
-            self.m_Parent.Activate(oData)
+        if self.m_Precondition:
+            self.m_Precondition.Activate(oData)
         if self.m_Child:
             for oNode in self.m_Child:
                 oNode.Activate(oData)
+        self.m_Actived=True
 
     def InitNode(self,oData):
         self.m_DataBase=oData
@@ -49,12 +44,17 @@ class CBTNode(object):
             return False
         if not self.CheckTimer():
             return False
+        if not (not self.m_Precondition or self.m_Precondition.Check()):
+            return False
+        if not self.DoEvaluate():
+            return False
+        return True
 
     def DoEvaluate(self):
         return True
 
     def Tick(self):
-        pass
+        return RESULTSTATE_ENDED
 
     def Clear(self):
         pass
